1935c859c7717b84019d5a2dd53b1ee16a467f16
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-09-28 19:30:07 -0400

    added task list

--- app.js
+++ app.js
@@ -22,5 +22,29 @@ function addTask(e) {
         alert('Add a Task');
     }
 
+    //create li element
+    const li = document.createElement('li');
+    //Add class
+    li.className = 'collection-item';
+    //Create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    //Create new link element 
+    const link = document.createElement('a');
+    //Add class
+    link.className = 'delete-item secondary-content';
+
+    //Add icon html
+    link.innerHTML = '<i class= "fa fa-remove"></i>';
+    //Append the link to li
+    li.appendChild(link);
+
+    //Append li to ul
+    //console.log(li);
+    taskList.appendChild(li);
+
+    //clear input
+    taskInput.value = '';
+
+
     e.preventDefault();
 }
