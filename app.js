dfbd74ad3a38e9b69c9cb093f3eb5234b6f81634
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-09-28 19:08:50 -0400

    add variable for dom manipulation

--- app.js
+++ app.js
@@ -4,3 +4,23 @@ const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+
+//Load all event Listeners
+loadEventListeners();
+
+
+//load all event listeners
+function loadEventListeners() {
+    //Add task event
+    form.addEventListener('submit', addTask);
+}
+
+
+//Add task
+function addTask(e) {
+    if (taskInput.value === '') {
+        alert('Add a Task');
+    }
+
+    e.preventDefault();
+}
