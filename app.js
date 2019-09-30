44ee91a6886372c8f0a63298f7fc63a9329fa786
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-09-29 20:54:53 -0400

    added filter tasks

--- app.js
+++ app.js
@@ -89,6 +89,9 @@ function filterTasks(e) {
     const text = e.target.value.toLowerCase();
     document.querySelectorAll('.collection-item').forEach(function (task) {
         const item = task.firstChild.textContent;
-        
+        if (item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block';
+
+        } 
     });
 }
