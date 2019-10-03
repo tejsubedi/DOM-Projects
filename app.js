af36c8eaf148a79edc40133ab697545975681fdc
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-10-02 22:33:58 -0400

    adding local storage

--- app.js
+++ app.js
@@ -36,6 +36,28 @@ function getTasks() {
     } else {
         tasks = JSON.parse(localStorage.getItem('tasks'));
     }
+
+    tasks.forEach(function (task) {
+        //create li element
+        const li = document.createElement('li');
+        //Add class
+        li.className = 'collection-item';
+        //Create text node and append to li
+        li.appendChild(document.createTextNode(taskInput.value));
+        //Create new link element 
+        const link = document.createElement('a');
+        //Add class
+        link.className = 'delete-item secondary-content';
+
+        //Add icon html
+        link.innerHTML = '<i class= "fa fa-remove"></i>';
+        //Append the link to li
+        li.appendChild(link);
+
+        //Append li to ul
+        //console.log(li);
+        taskList.appendChild(li);
+    })
 }
 
 
