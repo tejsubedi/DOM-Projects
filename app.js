7bd0883ae1bcea9141b64704f803b93b981b44d4
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-10-03 21:53:47 -0400

    adding delete function for local storage

--- app.js
+++ app.js
@@ -132,11 +132,13 @@ function removeTaskFromLocalStorage(taskItem) {
         tasks = JSON.parse(localStorage.getItem('tasks'));
     }
 
-    tasks.forEach(function(task, index) {
-        if(taskItem.textContent === task){
-            tasks.splice();
+    tasks.forEach(function (task, index) {
+        if (taskItem.textContent === task) {
+            tasks.splice(index, 1);
         }
-    })
+    });
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 //clear tasks
